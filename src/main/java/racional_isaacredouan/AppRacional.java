ac156cd2d19d3337222e153cc5380588a0791f0e
Isaac <jimenezmerino.isaac9871@gmail.com>
2020-12-10 14:33:42 +0100

    Cambios en Main

--- src/main/java/racional_isaacredouan/AppRacional.java
+++ src/main/java/racional_isaacredouan/AppRacional.java
@@ -5,15 +5,28 @@
  */
 package racional_isaacredouan;
 
+import java.util.Scanner;
+
 /**
  *
  * @author reduan
  */
 public class AppRacional {
-    
+
     public static void main(String[] args) {
-        
-        Racional racional1 = new Racional(2,5);
-        racional1.imprimirConsola();
+        Racional racional1 = new Racional();
+        Racional racional2 = new Racional();
+//Racional 1
+        racional1.setNumerador(5);
+        racional1.setDenominador(6);
+//Racional 2
+        racional2.setNumerador(5);
+        racional2.setDenominador(6);
+
+        Racional suma = new Racional();
+
+        System.out.println("Suma:");
+        suma.Suma(racional1.getNumerador(), racional1.getDenominador(), racional2.getNumerador(), racional2.getDenominador());
+
     }
 }
